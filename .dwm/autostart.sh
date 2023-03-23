4a7dd1100c9154c52134b534dc5f7b864961a03f
Gammaxane666 <mpmanish413@gmail.com>
2023-03-24 01:08:05 +0530

    little tweaks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-killall sxhkd
-while pgrep -u $UID -x sxhkd >/dev/null; do sleep 1; done
-sxhkd -m -1 -c ~/.config/suckless/dwm/sxhkd/sxhkdrc &
-
+#!/bin/sh
 # compositor
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom &
 
-# ~/.config/suckless/dwm/polybar/launch.sh &
+~/.config/suckless/dwm/polybar/launch.sh &
 
-dunst &
-mpd &
-nm-applet &
+exec nm-applet &
 ~/.fehbg &
