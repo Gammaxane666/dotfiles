47dd57b9924f08aac73f7e70a8b48ad186410ef9
Gammaxane666 <mpmanish413@gmail.com>
2023-03-25 22:15:05 +0530

    little tweaks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -7,7 +7,18 @@ picom --vsync &
 
 killall slstatus
 while pgrep -u $UID -x slstatus >/dev/null; do sleep 1; done
-slstatus
+slstatus &
 
+killall nm-applet
+while pgrep -u $UID -x nm-applet >/dev/null; do sleep 1; done
 nm-applet &
+
+killall volumeicon
+while pgrep -u $UID -x volumeicon >/dev/null; do sleep 1; done
+volumeicon &
+
+killall sxhkd
+while pgrep -u $UID -x sxhkd >/dev/null; do sleep 1; done
+sxhkd -m -1 -c ~/.config/suckless/dwm/sxhkd/sxhkdrc
+
 ~/.fehbg &
