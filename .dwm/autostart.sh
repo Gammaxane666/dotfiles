0986914166129f4f5ee3e01e8e790251e5b91689
Gammaxane666 <mpmanish413@gmail.com>
2023-03-26 16:01:51 +0530

    little tweaks

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+~/.fehbg &
+
+killall volumeicon
+while pgrep -u $UID -x volumeicon >/dev/null; do sleep 1; done
+volumeicon &
+
 # compositor
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
@@ -13,12 +19,6 @@ killall nm-applet
 while pgrep -u $UID -x nm-applet >/dev/null; do sleep 1; done
 nm-applet &
 
-killall volumeicon
-while pgrep -u $UID -x volumeicon >/dev/null; do sleep 1; done
-volumeicon &
-
 killall sxhkd
 while pgrep -u $UID -x sxhkd >/dev/null; do sleep 1; done
 sxhkd -m -1 -c ~/.config/suckless/dwm/sxhkd/sxhkdrc
-
-~/.fehbg &
