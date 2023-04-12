2de3ce51dfacaa0199d3c87fe24879c435f20406
gammaxane666 <mpmanish413@gmail.com>
2023-04-13 01:05:34 +0530

    Status bar for DWm

---
+++ .config/suckless/dwmblocks/blocks.def.h
@@ -0,0 +1,20 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
+    //{" 🐧 ", "~/.config/suckless/dwmblocks/scripts/kernel",	 360,		          2},
+	{"  ", "~/.config/suckless/dwmblocks/scripts/memory",	     3, 	              1},
+
+	{"  ", "~/.config/suckless/dwmblocks/scripts/upt",		     60,		          2},
+
+	//{" 📦 ", "~/.config/suckless/dwmblocks/scripts/pacupdate",  360,		          9},
+
+	//{" 🔊 ", "~/.config/suckless/dwmblocks/scripts/volume",      1,		              1},
+
+    {"   ", "~/.config/suckless/dwmblocks/scripts/battery",     1,                       1},
+
+	{"  ", "~/.config/suckless/dwmblocks/scripts/clock",	     60,	              0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
